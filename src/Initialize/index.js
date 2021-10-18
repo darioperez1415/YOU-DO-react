94d36f15a71cc7ae0ffe8327639fcbdafb3c1bb6
Dario Perez <darioperez1415@gmail.com>
2021-10-18 00:08:19 -0500

    Added authentication

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import { getTodos } from '../api/data/todoData';
 import TodoForm from '../components/TodoForm';
 import Navigation from '../components/Navigation';
 import Routes from '../routes';
+import SignIn from '../views/SignIn';
 
 const ContainerStyle = styled.div`
   width: 700px;
@@ -25,17 +28,40 @@ const ContainerStyle = styled.div`
 function Initialize() {
   const [todos, setTodos] = useState([]);
   const [editItem, setEditItem] = useState({});
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    getTodos().then(setTodos);
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        const userInfoObj = {
+          fullName: authed.displayName,
+          profileImage: authed.photoURL,
+          uid: authed.uid,
+          user: authed.email.split('@')[0],
+        };
+        setUser(userInfoObj);
+        getTodos(false).then(setTodos);
+      } else if (user || user === null) {
+        setUser(false);
+      }
+    });
+    // getTodos().then(setTodos);
   }, []);
 
   return (
     <ContainerStyle>
-      <Navigation />
-      <h1>YOU-DO-React</h1>
-      <TodoForm obj={editItem} setTodos={setTodos} setEditItem={setEditItem} />
-      <Routes obj={todos} setTodos={setTodos} setEditItem={setEditItem} />
+      {
+        user ? (
+          <>
+            <Navigation />
+            <h1>YOU-DO-React</h1>
+            <TodoForm obj={editItem} setTodos={setTodos} setEditItem={setEditItem} />
+            <Routes obj={todos} setTodos={setTodos} setEditItem={setEditItem} />
+          </>
+        ) : (
+          <SignIn user={user} />
+        )
+        }
     </ContainerStyle>
   );
 }
